a2b11ff3a859db71e79d686425f4ba16b0c5e73a
progand <lach@ukr.net>
2015-03-30 11:03:41 +0300

    [*] views structure

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import {Movies} from './model/Movies';
 import {Movie} from './model/Movie';
 import {MovieView} from './views/MovieView.js';
 import {SearchFormView} from './views/SearchFormView.js';
+import {SearchResultView} from './views/SearchResultView.js';
 
 global.app = function () {
     let movie = new Movie(),
         movies = new Movies(),
         searchFormView = new SearchFormView(),
-        movieView = new MovieView();
+        movieView = new MovieView(),
+        searchResultView = new SearchResultView();
 
     //ToDo: реалізація - пізніше
 };
